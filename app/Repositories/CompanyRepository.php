b97e7fccd33cdb6f5ad207689db767a17a59ca8d
Twenty2 <eaveasna1@gmail.com>
2023-07-01 21:24:23 +0700

    change

---
+++ app/Repositories/CompanyRepository.php
@@ -0,0 +1,12 @@
+<?php
+    namespace App\Repositories;
+
+    class CompanyRepository{
+        public function company_data(){
+            return [
+                1 => ['name' => 'Company 1'],
+                2 => ['name' => 'Company 2'],
+            ];
+        }
+    }
+?>
